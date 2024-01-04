c45028e80337a11bc095c597aecac12bd535eed4
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-01-04 17:51:49 +0300

    Refactored validation code in blog-validator.ts and post-validator.ts for better readability

--- src/validators/blog-validator.ts
+++ src/validators/blog-validator.ts
@@ -1,8 +1,21 @@
 import {body} from "express-validator";
 
-const nameValidation = body('name').trim().isString().isLength({min: 1, max: 15}).withMessage("Incorrect name");
-const descriptionsValidation = body('description').trim().isString().isLength({min: 1, max: 500}).withMessage("Incorrect description");
-const websiteUrlValidation = body('websiteUrl').trim().isString().isLength({min: 1, max: 100}).isURL()
+const nameValidation = body('name')
+    .trim()
+    .isString()
+    .isLength({min: 1, max: 15})
+    .withMessage("Incorrect name");
+
+const descriptionsValidation = body('description')
+    .trim()
+    .isString()
+    .isLength({min: 1, max: 500})
+    .withMessage("Incorrect description");
+
+const websiteUrlValidation = body('websiteUrl')
+    .trim()
+    .isString()
+    .isLength({min: 1, max: 100})
     .matches("^https://([a-zA-Z0-9_-]+\\.)+[a-zA-Z0-9_-]+(\\/[a-zA-Z0-9_-]+)*\\/?$\n").withMessage("Incorrect websiteUrl");
 
 export const blogValidation = (optional:boolean = false) => {
