c33b4fb3591c742f2b1408d5b3aab283df88d2ca
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-01-17 22:13:42 +0300

    fixed to test

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,18 +1,14 @@
 import {LoginModel} from "../models/auth/input";
-import {userService} from "./user-service";
 import bcrypt from "bcrypt"
+import {JwtService} from "../application/jwt-service";
 
 export class AuthService {
     static async login(credentials: LoginModel) {
-        const user = await userService.getUserByEmailOrLogin(credentials.loginOrEmail)
+        const user = await JwtService.login(credentials)
         if(!user){
             return null
         }
-        const isPasswordValid = await this._validatePassword(credentials.password, user.passwordSalt, user.passwordHash)
-        if(!isPasswordValid){
-            return null
-        }
-        return isPasswordValid
+        return user
     }
 
     static async _validatePassword(password: string, salt: string, hash: string) {
