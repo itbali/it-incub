02c9f35e8c784cb038b42a6a886aa48393cf8071
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-01-25 23:17:52 +0300

    email-confirmation-validator moved from body to query

--- src/validators/email-confirmation-validator.ts
+++ src/validators/email-confirmation-validator.ts
@@ -1,8 +1,8 @@
-import {body} from "express-validator";
+import {query} from "express-validator";
 import {inputModelValidation} from "../middlewares/input-model-validation/input-model-validation";
 import {checkEmailJwtCode} from "./check-email-jwt-code";
 
-const codeValidator = body('code')
+const codeValidator = query('code')
     .custom((value:string)=>{
         console.log(value)
         return true
