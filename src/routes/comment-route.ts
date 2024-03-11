030bc087d80955da5a17747354b603ca8d0fd8d0
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-03-11 23:26:31 +0100

    ht11 added getUserFromTokenMiddleware

--- src/routes/comment-route.ts
+++ src/routes/comment-route.ts
@@ -1,12 +1,12 @@
 import {Router} from "express";
-import {jwtMiddleware} from "../middlewares/auth/jwt-middleware";
+import {getUserFromTokenMiddleware, jwtMiddleware} from "../middlewares/auth/jwt-middleware";
 import {commentValidation} from "../validators/comment-validator";
 import {commentController} from "../composition-roots/comment-composition";
 import {likeStatusValidator} from "../validators/like-status-validator";
 
 export const commentRoute = Router();
 
-commentRoute.get("/:id", commentController.getComment.bind(commentController))
+commentRoute.get("/:id", getUserFromTokenMiddleware, commentController.getComment.bind(commentController))
 commentRoute.put("/:id", jwtMiddleware, commentValidation(), commentController.updateComment.bind(commentController))
 commentRoute.put("/:id/like-status", jwtMiddleware, likeStatusValidator(), commentController.likeComment.bind(commentController))
 commentRoute.delete("/:id", jwtMiddleware, commentController.deleteComment.bind(commentController))
