10990acd3db81fea70f33ff360f4796edc170b56
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-03-16 21:41:06 +0100

    ht12 updated compositions. merged previous brunch into new

--- src/composition-roots/post-composition.ts
+++ src/composition-roots/post-composition.ts
@@ -3,12 +3,6 @@ import {PostRepository} from "../repositories/post-repository";
 import {PostService} from "../services/post-service";
 import {PostController} from "../controllers/post-controller";
 import {Container} from "inversify";
-import {CommentRepository} from "../repositories/comment-repository";
-import {CommentService} from "../services/comment-service";
-import {UserRepository} from "../repositories/user-repository";
-import {BlogService} from "../services/blog-service";
-import {BlogRepository} from "../repositories/blog-repository";
-import {JwtService} from "../application/jwt-service";
 import {BlogService} from "../services/blog-service";
 import {BlogRepository} from "../repositories/blog-repository";
 import {CommentRepository} from "../repositories/comment-repository";
@@ -28,12 +22,3 @@ postContainer.bind(PostService).to(PostService);
 postContainer.bind(PostController).to(PostController);
 
 export const postController = postContainer.resolve(PostController);
-const postRepository = new PostRepository();
-const blogRepository = new BlogRepository();
-const commentRepository = new CommentRepository();
-const jwtService = new JwtService();
-const userRepository = new UserRepository(jwtService);
-const blogService = new BlogService(blogRepository, postRepository)
-const commentService = new CommentService(commentRepository);
-const postService = new PostService(postRepository, blogService, commentRepository, userRepository);
-export const postController = new PostController(postService, commentService);
