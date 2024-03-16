4771b127689a313a3f7963927315a8d135bfc32b
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-03-16 21:49:47 +0100

    ht12 fixed postMapper

--- src/models/posts/mappers/postMapper.ts
+++ src/models/posts/mappers/postMapper.ts
@@ -15,7 +15,7 @@ export const postMapper = (post: WithId<PostDBType>, userId?: string): PostVM =>
             likesCount: post.likesInfo.likesCount,
             dislikesCount: post.likesInfo.dislikesCount,
             myStatus: post.likesInfo.usersLiked?.find(like => like.userId === userId)?.likeStatus || 'none',
-            newestLikes: post.likesInfo.usersLiked?.filter(like => like.likeStatus === 'like').map(like => like.userId).slice(0,2) || [],
+            newestLikes: post.likesInfo.usersLiked?.filter(like => like.likeStatus === 'Like').map(like => like.userId).slice(0,2) || [],
             usersLiked: post.likesInfo.usersLiked
         }
     });
